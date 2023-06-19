93a05134489225143478d3cea416f3879c5b5eff
Uallesson Nunes Ivo <uallessons@gmail.com>
2023-06-19 20:12:16 -0300

    Add project files.

---
+++ NetAuth/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetAuth.Dtos;
+using NetAuth.Models;
+
+namespace NetAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User User = new User();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto requres)
+        {
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(requres.PasswordHash);
+
+            User.UserName = requres.UserName;
+            User.PasswordHash = passwordHash;
+
+            return Ok(User);
+        }
+    }
+}
---
+++ NetAuth/Dtos/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace NetAuth.Dtos
+{
+    public class UserDto
+    {
+        public required string UserName { get; set; } = string.Empty;
+        public required string PasswordHash { get; set; } = string.Empty;
+    }
+}
