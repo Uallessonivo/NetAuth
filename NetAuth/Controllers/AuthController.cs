5120e2fc54db8af6a86ca0d8b1245d373e88ba4c
Uallesson Nunes Ivo <uallessons@gmail.com>
2023-06-19 20:58:50 -0300

    feat : now generating the jwt

--- NetAuth/Controllers/AuthController.cs
+++ NetAuth/Controllers/AuthController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using NetAuth.Dtos;
 using NetAuth.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace NetAuth.Controllers
 {
@@ -9,16 +13,64 @@ namespace NetAuth.Controllers
     public class AuthController : ControllerBase
     {
         public static User User = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         [HttpPost("register")]
-        public ActionResult<User> Register(UserDto requres)
+        public ActionResult<User> Register(UserDto requrest)
         {
-            string passwordHash = BCrypt.Net.BCrypt.HashPassword(requres.PasswordHash);
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(requrest.Password);
 
-            User.UserName = requres.UserName;
+            User.UserName = requrest.UserName;
             User.PasswordHash = passwordHash;
 
             return Ok(User);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDto requrest)
+        {
+            if (User.UserName != requrest.UserName)
+            {
+                return BadRequest("User not found.");
+
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(requrest.Password, User.PasswordHash))
+            {
+                return BadRequest("Wrong email or password");
+            }
+
+            string token = CreateToken(User);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: cred
+            );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
         }
     }
 }
--- NetAuth/Dtos/UserDto.cs
+++ NetAuth/Dtos/UserDto.cs
@@ -3,6 +3,6 @@
     public class UserDto
     {
         public required string UserName { get; set; } = string.Empty;
-        public required string PasswordHash { get; set; } = string.Empty;
+        public required string Password { get; set; } = string.Empty;
     }
 }
